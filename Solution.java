e9781d4531d655f27e54660ab66d7ee8f7294c48
sdim-ua <ingersol@yandex.ua>
2019-06-14 11:52:03 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,32 @@
+/* 
+Пятая правильная «цепочка наследования»
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class House extends Asset{
+
+    }
+
+    public class Cat extends Animal {
+
+    }
+
+    public class Car extends Asset {
+
+    }
+
+    public class Dog extends Animal {
+
+    }
+
+    public class Animal {
+
+    }
+
+    public class Asset {
+
+    }
+}
